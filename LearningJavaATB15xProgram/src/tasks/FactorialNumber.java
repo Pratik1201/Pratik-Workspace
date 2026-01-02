e6e5d64b928cce3aa67def169c0dd66a56f5d994
Pratik1201 <pratikhsonawane@gmail.com>
2026-01-02 21:03:50 +0530

    Java program for factorial number

---
+++ LearningJavaATB15xProgram/src/tasks/FactorialNumber.java
@@ -0,0 +1,17 @@
+package tasks;
+
+import java.util.Scanner;
+
+public class FactorialNumber {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Enter a number");
+		int num, fact=1;
+		num = sc.nextInt();
+		for(int i=1;i<=num;i++) {
+			fact = fact*i;
+		}
+		System.out.println("The factorial of given number is: " + fact);
+	}
+
+}
