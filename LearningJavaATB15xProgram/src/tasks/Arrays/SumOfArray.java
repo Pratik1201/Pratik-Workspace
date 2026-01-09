161aff4ed482fdb0a19b9c7eaca43400073b1913
Pratik1201 <pratikhsonawane@gmail.com>
2026-01-09 21:43:53 +0530

    program of sum of array

---
+++ LearningJavaATB15xProgram/src/tasks/Arrays/SumOfArray.java
@@ -0,0 +1,13 @@
+package tasks.Arrays;
+
+public class SumOfArray {
+public static void main(String[] args) {
+	int arr[] = new int[] {12,01,19,94};
+	int sum = 0;
+	
+	for (int i=0;i<arr.length;i++) {
+		sum = arr[i]+sum;
+	}
+	System.out.println("Sum of array is:- " +sum);
+}
+}
