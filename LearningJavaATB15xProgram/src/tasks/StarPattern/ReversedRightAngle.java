18ebcf132aa1ccfee59ce1b3d05027dac4c77420
Pratik1201 <pratikhsonawane@gmail.com>
2026-01-20 19:23:03 +0530

    Java program for reversed right angled triangle

---
+++ LearningJavaATB15xProgram/src/tasks/StarPattern/ReversedRightAngle.java
@@ -0,0 +1,19 @@
+package tasks.StarPattern;
+
+import java.util.Scanner;
+
+public class ReversedRightAngle {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int num = sc.nextInt();
+		
+		for(int i= num;i>=1;i--) {
+			for (int j=1;j<=i;j++) {
+				System.out.print(" * ");
+			}
+			System.out.println();
+		}
+	}
+
+}
