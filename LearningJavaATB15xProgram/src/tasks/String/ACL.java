35ced7750fa85f6d38e65026d5ae82f893d5d503
Pratik1201 <pratikhsonawane@gmail.com>
2026-02-09 19:03:16 +0530

    Java ACL Program

---
+++ LearningJavaATB15xProgram/src/tasks/String/ACL.java
@@ -0,0 +1,22 @@
+package tasks.String;
+
+import java.util.Scanner;
+
+public class ACL {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a string");
+		String s = sc.nextLine();
+		StringBuilder sb = new StringBuilder();
+		for(int i=0;i<s.length();i++) {
+			if(i%2==0) {
+				sb.append(Character.toLowerCase(s.charAt(i)));
+			}
+			else {
+				sb.append(Character.toUpperCase(s.charAt(i)));
+			}
+		}
+		System.out.println(sb);
+	}
+
+}
