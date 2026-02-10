dc7420e71ae57603d4f4b28701090c7963567572
Pratik1201 <pratikhsonawane@gmail.com>
2026-02-10 17:12:46 +0530

    Coding question

---
+++ LearningJavaATB15xProgram/src/coding_task/Person.java
@@ -0,0 +1,22 @@
+package coding_task;
+
+public class Person {
+	String name;
+	int age;
+	
+	Person (String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	void display() {
+		System.out.println("Name: " + name  + "," +" Age: " + age);
+	}
+	public static void main(String[] args) {
+		Person p1=new Person("John", 25);
+		Person p2 = new Person("Alice", 30);
+		p1.display();
+		p2.display();
+	}
+
+}
+ 
