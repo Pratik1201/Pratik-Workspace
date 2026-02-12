18e85a8d13864a850c0910717e446606402c7c8c
Pratik1201 <pratikhsonawane@gmail.com>
2026-02-12 21:56:39 +0530

    Added a string program

---
+++ LearningJavaATB15xProgram/src/tasks/String/SubSetOfString.java
@@ -0,0 +1,23 @@
+package tasks.String;
+
+
+public class SubSetOfString {
+	public static void main(String[] args) {
+		String s = "BAT";
+		int len = s.length();
+		int temp = 0;
+		String arr [] = new String [len*(len+1)/2];
+		
+		for (int i =0;i<len;i++) {
+			for(int j=i;j<len;j++) {
+				arr[temp] = s.substring(i,j+1);
+				temp++;
+			}
+		}
+		System.out.println("All subset of string is ");
+		for(int i=0;i<arr.length;i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+}
