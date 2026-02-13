029ee612f252bd2563b8642429bf0d87ef54cf40
Pratik1201 <pratikhsonawane@gmail.com>
2026-02-13 22:47:29 +0530

    13th Feb 2026

---
+++ LearningJavaATB15xProgram/src/collections/List_001.java
@@ -0,0 +1,18 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class List_001 {
+	public static void main(String[] args) {
+		List fruits = List.of("Mango", "Apple", "Dragon-Fruit", "Banana");
+		System.out.println(fruits);
+		
+		List arrayList = new ArrayList();
+		arrayList.add("Orange");
+		arrayList.add("Watermelon");
+		
+		System.out.println(arrayList);
+	}
+
+}
