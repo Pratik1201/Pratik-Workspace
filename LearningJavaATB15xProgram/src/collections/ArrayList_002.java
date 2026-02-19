8f7f2f9960335b8db16a59a7c7f25ef0c7a321ac
Pratik1201 <pratikhsonawane@gmail.com>
2026-02-19 18:47:26 +0530

    added Arraylist java program

---
+++ LearningJavaATB15xProgram/src/collections/ArrayList_002.java
@@ -0,0 +1,33 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayList_002 {
+	public static void main(String[] args) {
+		List list = new ArrayList();
+		list.add("Pratik");
+		list.add("2");
+		list.add("Sumit");
+		list.add("3");
+		list.add("Chalisgaon");
+		
+
+        System.out.println(list.size());
+        System.out.println(list.isEmpty());
+        System.out.println(list.contains("1"));
+        System.out.println(list.contains(1));
+        System.out.println(list.indexOf("3"));
+        System.out.println(list.lastIndexOf("3"));
+
+
+        System.out.println(list);
+
+        System.out.println(" --- ");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+		
+	}
+
+}
